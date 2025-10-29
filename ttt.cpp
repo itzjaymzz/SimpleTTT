8c550c2e99c628a2ff00671685fe4cea969b09ca
itzjaymzz <jamesisquick@gmail.com>
2025-10-29 08:09:03 -0600

    worst thing ever

--- ttt.cpp
+++ ttt.cpp
@@ -7,48 +7,38 @@
 using namespace std;
 
 
-char wincheck(const vector<string> board) {
+string wincheck(const vector<string> board) {
     // 1. Check Rows
     for (int i = 0; i < 9; i += 3) {
         if (board[i] != "--" && board[i] == board[i+1] && board[i] == board[i+2]){
-        	 return board[];
+        	 return board[i];
         } 
     }
     // 2. Check Columns
     for (int i = 0; i < 3; ++i) {
         if (board[i] != "--" && board[i] == board[i+3] && board[i] == board[i+6]){ 
-        	return false;
+        	return board[i];
         }	
     }
     // 3. Check Diagonals
     if (board[0] != "--" && board[0] == board[4] && board[0] == board[8]) {
-    	return false;
+    	return board[0];
     }	
     if (board[2] != "--" && board[2] == board[4] && board[2] == board[6]) {
-    return false;
+    	return board[2];
     }
-    return true;
-}
-//Prints the board thats all it does rn
-void print_board(vector<string> pos){  
-	printf("\t1\t2\t3  \n\n"
-	"A\t%s \t%s \t%s\n\n" 
-	"B\t%s \t%s \t%s\n\n" 
-	"C\t%s \t%s \t%s\n\n", 
-	pos[0].c_str(), pos[1].c_str(), pos[2].c_str(),  
-	pos[3].c_str(), pos[4].c_str(), pos[5].c_str(),  
-	pos[6].c_str(), pos[7].c_str(), pos[8].c_str());
+    return "A";
 }
 
 int translate_move(string choice) {
 	if (choice.length() != 2){
-		printf("Erm I don't think thats a valid move try again\n");
+		throw runtime_error("Erm I don't think thats a valid move try again");
 	}
 	if (choice[0] != 'A' && choice[0] != 'B' && choice[0] != 'C'){
-		printf("Erm I don't think thats a valid move try again\n");
+		throw runtime_error("Erm I don't think thats a valid move try again");
 	}
 	if (choice[1] < '1' || choice [1] > '3'){
-		printf("Erm I don't think thats a valid move try again\n");				
+		throw runtime_error("Erm I don't think thats a valid move try again");				
 	}	 
 	char row = toupper(choice[0]);
 	int collumn = choice[1] - '0';
@@ -63,6 +53,17 @@ int translate_move(string choice) {
 		index = 6;
 	}
 	return index + (collumn -1);
+}
+
+//Prints the board thats all it does rn
+void print_board(vector<string> pos){  
+	printf("\t1\t2\t3  \n\n"
+	"A\t%s \t%s \t%s\n\n" 
+	"B\t%s \t%s \t%s\n\n" 
+	"C\t%s \t%s \t%s\n\n", 
+	pos[0].c_str(), pos[1].c_str(), pos[2].c_str(),  
+	pos[3].c_str(), pos[4].c_str(), pos[5].c_str(),  
+	pos[6].c_str(), pos[7].c_str(), pos[8].c_str());
 }
 
 
@@ -90,7 +91,7 @@ int main() {
 	print_board(board);
 	int counter = 1;
 	string userchoice;
-	while(wincheck(board)){
+	while(wincheck(board) == "A"){
 		printf("For move #%d where would you like to go? ", counter);
 		cin >> userchoice;
 		int index = translate_move(userchoice);
@@ -103,8 +104,12 @@ int main() {
 		}	
 		counter++;
 	}
-	if(wincheck(board) == false){
-		printf("Someone lost!\n");
+	if(wincheck(board) == "X"){
+		printf("O lost!\n");
 	}
+	if(wincheck(board) == "O"){
+		printf("X lost!\n");
+	}
+
 	return 0;
 }
